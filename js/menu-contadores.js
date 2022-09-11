e0a2953bfe6585501169e5ccd7ffddbc2ca33652
IzakCancino <cancinopalestinaizakdaniel@gmail.com>
2022-09-10 20:48:43 -0500

    Product class and objects

--- js/menu-contadores.js
+++ js/menu-contadores.js
@@ -1,8 +1,39 @@
-const tarjetas = document.querySelectorAll(".tarjeta");
+let cards = document.querySelectorAll(".tarjeta");
+// Convierte la lista de nodos en un Array
+cards = [].slice.call(cards);
 
-tarjetas.forEach(elmnt => {
-    //este es el menos
-    elmnt.children[2].children[0];
-});
+// Botones menos
+let less = cards.map((elmnt => {
+    let btn = elmnt.children[2].children[0];
 
-console.log(tarjetas);
+    btn.addEventListener("click", () => {
+        let cnt = elmnt.children[2].children[1];
+        if (parseInt(cnt.innerText) > 0) {  
+            cnt.innerText = parseInt(cnt.innerText) - 1;
+        }
+    });
+
+    return btn;
+}));
+
+// Botones mas
+let more = cards.map((elmnt => {
+    let btn = elmnt.children[2].children[2];
+
+    btn.addEventListener("click", () => {
+        let cnt = elmnt.children[2].children[1];
+        if (parseInt(cnt.innerText) < 15) {  
+            cnt.innerText = parseInt(cnt.innerText) + 1;
+        } else {
+            alert("Lo sentimos, no es posible exceder la cantidad de 15 elementos de un mismo producto");
+        }
+
+
+        
+
+
+
+    });
+
+    return btn;
+}));
